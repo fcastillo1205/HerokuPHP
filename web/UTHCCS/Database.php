a918bda397da07831df52bf8e593c73edf849c43
fcastillo1205 <fers.cas12599@gmail.com>
2021-11-26 19:21:34 -0600

    cambiosDB

--- web/UTHCCS/Database.php
+++ web/UTHCCS/Database.php
@@ -4,13 +4,14 @@
         private $database_name = "d61hs4tjfp8q4k";
         private $username = "epzhrcobjntjle";
         private $password = "ab87578d1e2b72a242708a14c54f27b519ab1a700b1641ab215e7f544bfcda3d";
+        private $port = "5432";
 
         public $conn;
 
         public function getConnection(){
             $this->conn = null;
             try{
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn = new PDO("pgsql:host=" . $this->host . ";port=" . $this->port  . ";dbname=" . $this->database_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
                
             }catch(PDOException $exception)

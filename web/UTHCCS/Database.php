5772e47210c93ee9de91d6228be75fb06be507e3
fcastillo1205 <fers.cas12599@gmail.com>
2021-11-26 19:15:48 -0600

    db

--- web/UTHCCS/Database.php
+++ web/UTHCCS/Database.php
@@ -1,9 +1,9 @@
 <?php 
     class Database {
-        private $host = "localhost";
-        private $database_name = "uthccs";
-        private $username = "root";
-        private $password = "";
+        private $host = "ec2-35-168-80-116.compute-1.amazonaws.com";
+        private $database_name = "d61hs4tjfp8q4k";
+        private $username = "epzhrcobjntjle";
+        private $password = "ab87578d1e2b72a242708a14c54f27b519ab1a700b1641ab215e7f544bfcda3d";
 
         public $conn;
 

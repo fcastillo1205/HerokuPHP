dbf96f713a3aff2b468ec4be6f9c2ea54556af76
fcastillo1205 <fers.cas12599@gmail.com>
2021-11-26 19:02:52 -0600

    prueba form

--- web/views/articulos.php
+++ web/views/articulos.php
@@ -1,39 +1,45 @@
-< !doctype html>
-  <html lang="en">
+<html lang="en">
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Inventario</title>
-    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistema de Inventario</title>
+  <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="/stylesheets/main.css" />
+</head>
 
-  <body>
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-          <div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Menu</span><span
-                class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a
-              class="navbar-brand" href="#">UTHCSS</a></div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Inicio</a></li>
-                <li><a href="/views/articulos.php">Articulos</a></li>
-                <li><a href="/views/bodegas.php">Bodegas</a></li>
-                <li><a href="/views/kardex.php">Kardex</a></li>
-              </ul>
-            </div>
-      </div>
-    </nav>
-    <div class="text-center">
-      <div class="container"><img src="/images/inicio.png" alt="..." class="img-thumbnail" style="width: 30%;">
-        <h1>¡Bienvenido al sistema de inventario !</h1>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+          data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Menu</span><span
+            class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a
+          class="navbar-brand" href="#">UTHCSS</a></div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Inicio</a></li>
+          <li><a href="/views/articulos.php">Articulos</a></li>
+          <li><a href="/views/bodegas.php">Bodegas</a></li>
+          <li><a href="/views/kardex.php">Kardex</a></li>
+        </ul>
       </div>
     </div>
-  </body>
+  </nav>
 
-  </html>
+  <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</body>
+
+</html>

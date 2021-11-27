2f9aba97386135c4ffa122519e84cc4855eb932c
fcastillo1205 <fers.cas12599@gmail.com>
2021-11-26 22:48:18 -0600

    api

--- web/UTHCCS/Database.php
+++ web/UTHCCS/Database.php
@@ -1,17 +1,17 @@
 <?php 
     class Database {
-        private $host = "ec2-35-168-80-116.compute-1.amazonaws.com";
-        private $database_name = "d61hs4tjfp8q4k";
-        private $username = "epzhrcobjntjle";
-        private $password = "ab87578d1e2b72a242708a14c54f27b519ab1a700b1641ab215e7f544bfcda3d";
-        private $port = "5432";
+        private $host = "localhost";
+        private $database_name = "id15605615_programacionweb2";
+        private $username = "id15605615_localhost";
+        private $password = "ProgramacionWeb_2";
+        //private $port = "5432";
 
         public $conn;
 
         public function getConnection(){
             $this->conn = null;
             try{
-                $this->conn = new PDO("pgsql:host=" . $this->host . ";port=" . $this->port  . ";dbname=" . $this->database_name, $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
                
             }catch(PDOException $exception)
